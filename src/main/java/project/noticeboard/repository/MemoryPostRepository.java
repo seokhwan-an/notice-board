f84b7f29129ccd88650e84eeda25955a3e1d1ca2
seokhwan-an <asok369258@naver.com>
2022-01-27 00:05:23 +0900

    PostRepository 메모리 구현체 생성

---
+++ src/main/java/project/noticeboard/repository/MemoryPostRepository.java
@@ -0,0 +1,41 @@
+package project.noticeboard.repository;
+
+import project.noticeboard.domain.Post;
+
+import java.util.*;
+
+public class MemoryPostRepository implements PostRepository{
+    private static Map<Long, Post> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Post save(Post post) {
+        post.setId(++sequence);
+        store.put(post.getId(), post);
+        return post;
+    }
+
+    @Override
+    public Optional<Post> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Post> findByTitle(String title) {
+        return store.values().stream()
+                .filter(post -> post.getTitle().equals(title))
+                .findAny();
+    }
+
+    @Override
+    public Optional<Post> findByWriter(String writer) {
+        return store.values().stream()
+                .filter(post -> post.getTitle().equals(writer))
+                .findAny();
+    }
+
+    @Override
+    public List<Post> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
