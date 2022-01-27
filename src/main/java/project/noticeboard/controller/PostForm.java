d1a23e0908cace294258537284c0ccef1a2c0e5e
seokhwan-an <asok369258@naver.com>
2022-01-28 03:48:14 +0900

    PostForm 코드 추가(게시글 입력받기 위한)

---
+++ src/main/java/project/noticeboard/controller/PostForm.java
@@ -0,0 +1,28 @@
+package project.noticeboard.controller;
+
+public class PostForm {
+    private String title;
+    private String writer;
+    private String body;
+
+    public String getTitle(){
+        return title;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getWriter(){
+        return writer;
+    }
+    public void setWriter(String writer){
+        this.writer = writer;
+    }
+
+    public String getBody(){
+        return body;
+    }
+    public void setBody(String body){
+        this.body = body;
+    }
+}
