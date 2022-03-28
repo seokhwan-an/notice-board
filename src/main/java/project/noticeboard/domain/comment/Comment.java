017611fbd84b456e04d90abb728936551d440f45
seokhwan-an <asok369258@naver.com>
2022-03-28 21:23:21 +0900

    댓글 domain 수정

--- src/main/java/project/noticeboard/domain/comment/Comment.java
+++ src/main/java/project/noticeboard/domain/comment/Comment.java
@@ -2,25 +2,22 @@ package project.noticeboard.domain.comment;
 
 import lombok.Getter;
 import lombok.Setter;
+import project.noticeboard.domain.Timestamped;
 import project.noticeboard.domain.post.Post;
 
 import javax.persistence.*;
 
+@Entity
 @Getter @Setter
-public class Comment {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+public class Comment extends Timestamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column
     private String content;
 
-    @Column
-    private String created_at;
-
-    @Column
-    private String update_at;
-
-    @ManyToOne
-    @JoinColumn(name = "postid")
+    @ManyToOne(targetEntity = Post.class)
+    @JoinColumn(name = "post_id")
     private Post post;
 }
