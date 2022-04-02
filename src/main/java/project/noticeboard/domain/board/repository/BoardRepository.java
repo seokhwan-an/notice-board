8b3f966e9918cfe40017ab3c28addb730fca2e70
seokhwan-an <asok369258@naver.com>
2022-04-03 01:04:53 +0900

    BoardRepository 추가

---
+++ src/main/java/project/noticeboard/domain/board/repository/BoardRepository.java
@@ -0,0 +1,8 @@
+package project.noticeboard.domain.board.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import project.noticeboard.domain.board.Board;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+}
