546be06d8737caba8b9ba7e567a31c96b569a01d
seokhwan-an <asok369258@naver.com>
2022-04-03 01:03:57 +0900

    CommentRepository 수정

--- src/main/java/project/noticeboard/domain/comment/repository/CommentRepository.java
+++ src/main/java/project/noticeboard/domain/comment/repository/CommentRepository.java
@@ -1,14 +1,9 @@
 package project.noticeboard.domain.comment.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import project.noticeboard.domain.comment.Comment;
-import project.noticeboard.domain.post.Post;
 
-import java.util.List;
-import java.util.Optional;
 
-public interface CommentRepository {
-    void save(Comment comment);
-    Optional<Comment> findById(long id);
-    List<Comment> findAll();
-    void delete(Long id);
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
 }
