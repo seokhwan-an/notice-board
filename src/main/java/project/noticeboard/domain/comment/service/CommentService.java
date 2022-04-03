590091f45c68739884654dfc81522ad3bef6389b
seokhwan-an <asok369258@naver.com>
2022-04-03 09:42:16 +0900

    PostService 수정

--- src/main/java/project/noticeboard/domain/comment/service/CommentService.java
+++ src/main/java/project/noticeboard/domain/comment/service/CommentService.java
@@ -1,20 +1,29 @@
 package project.noticeboard.domain.comment.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import project.noticeboard.domain.comment.Comment;
 import project.noticeboard.domain.comment.repository.CommentRepository;
 
-import java.util.List;
 
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+
+@RequiredArgsConstructor
 public class CommentService {
     private final CommentRepository commentRepository;
     //DI 가능하게
-    public CommentService(CommentRepository commentRepository){
-        this.commentRepository = commentRepository;
-    }
+
 
     public Long save(Comment comment){
         commentRepository.save(comment);
         return comment.getId();
+    }
+    public Optional<Comment> findbyId(Long id){
+        Optional<Comment> get = Optional.ofNullable(commentRepository.findById(id)).get();
+        return get;
     }
 
     public List<Comment> findComments(){
@@ -22,7 +31,8 @@ public class CommentService {
         return result;
     }
 
+    @Transactional
     public void delete(Long id){
-        commentRepository.delete(id);
+        commentRepository.deleteById(id);
     }
 }
