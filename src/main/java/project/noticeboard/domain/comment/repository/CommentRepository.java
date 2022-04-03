0a16f66e77322badc0e5ebc87d0097c5a6ce10a1
seokhwan-an <asok369258@naver.com>
2022-04-03 09:35:44 +0900

    CommentRepositoryRepository 인터페이스를 JpaRespository 활용해 수정

--- src/main/java/project/noticeboard/domain/comment/repository/CommentRepository.java
+++ src/main/java/project/noticeboard/domain/comment/repository/CommentRepository.java
@@ -1,9 +1,10 @@
 package project.noticeboard.domain.comment.repository;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
 import project.noticeboard.domain.comment.Comment;
 
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-
+public interface CommentRepository extends JpaRepository<Comment, Long>{
 }
