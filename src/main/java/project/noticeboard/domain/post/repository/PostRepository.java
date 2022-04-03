c36806e3f27eec89e33e385f40d26b545ac26d4f
seokhwan-an <asok369258@naver.com>
2022-04-03 09:35:03 +0900

    PostRepository 인터페이스를 JpaRespository 활용해 수정

--- src/main/java/project/noticeboard/domain/post/repository/PostRepository.java
+++ src/main/java/project/noticeboard/domain/post/repository/PostRepository.java
@@ -3,8 +3,5 @@ package project.noticeboard.domain.post.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import project.noticeboard.domain.post.Post;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface PostRepository extends JpaRepository<Post, Long> {
 }
