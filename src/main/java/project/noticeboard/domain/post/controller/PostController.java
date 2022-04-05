d776a673b5a2576d01c3b11b1b8a42c0c1c420e7
seokhwan-an <asok369258@naver.com>
2022-04-05 18:20:04 +0900

    PostController 게시글 CRD 기능 수정

--- src/main/java/project/noticeboard/domain/post/controller/PostController.java
+++ src/main/java/project/noticeboard/domain/post/controller/PostController.java
@@ -4,52 +4,62 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import project.noticeboard.domain.board.Board;
+import project.noticeboard.domain.board.repository.service.BoardService;
 import project.noticeboard.domain.post.Post;
 import project.noticeboard.domain.post.service.PostService;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
+@RequestMapping("/board")
 @Controller
 public class PostController {
 
     private final PostService postService;
+    private final BoardService boardService;
 
-    @GetMapping(value = "/posts/new")
+    @GetMapping(value = "/new-posts")
     public String CreateForm(){
         return "posts/createPostForm";
     }
 
     // 게시글 데이터 저장
-    @PostMapping(value = "/posts/new")
-    public String create(PostForm postForm){
+    @PostMapping(value = "/new-posts")
+    public String create(PostForm postForm, @RequestParam String type) throws UnsupportedEncodingException {
         Post post = new Post();
+        type= URLEncoder.encode(type, "UTF-8");
         post.setTitle(postForm.getTitle());
         post.setWriter(postForm.getWriter());
         post.setBody(postForm.getBody());
         postService.save(post);
-        return "redirect:/posts";
+        return "redirect:/board?type="+ type;
     }
     // 게시글 조회
-    @GetMapping("/posts")
-    public String showAll(Model model){
+    @GetMapping()
+    public String showAll(Model model, @RequestParam String type) {
         List<Post> posts = postService.findPosts();
         model.addAttribute("posts",posts);
         return "posts/posts";
     }
     // 게시글 상세페이지
     @GetMapping("/posts/{no}")
-    public String detail(@PathVariable("no") Long no, Model model){
+    public String detail(@PathVariable("no") Long no, @RequestParam String type, Model model){
         Optional<Post> post = postService.findbyId(no);
+        log.info("type = " + type );
         post.ifPresent(value -> model.addAttribute("post",value));
+        model.addAttribute("type", type);
         return "posts/detail";
     }
     // 게시글 삭제
     @PostMapping("/posts/{no}")
-    public String delete(@PathVariable("no") Long no){
+    public String delete(@PathVariable("no") Long no, @RequestParam String type) throws UnsupportedEncodingException {
         postService.delete(no);
-        return "redirect:/";
+        type= URLEncoder.encode(type, "UTF-8");
+        return "redirect:/board?type="+ type;
     }
 
     @GetMapping("/posts/edit/{no}")
@@ -67,6 +77,6 @@ public class PostController {
         edit_post.setWriter(postForm.getWriter());
         edit_post.setBody(postForm.getBody());
         postService.save(edit_post);
-        return "redirect:/posts/{no}";
+        return "redirect: posts/{no}";
     }
 }
