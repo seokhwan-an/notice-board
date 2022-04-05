1329cf4eee2f05833ab4e267c4480bb4467e264b
seokhwan-an <asok369258@naver.com>
2022-04-05 09:43:25 +0900

    MainController 메인페이지 이동시 board 객체 추가

--- src/main/java/project/noticeboard/domain/main/MainController.java
+++ src/main/java/project/noticeboard/domain/main/MainController.java
@@ -1,14 +1,23 @@
 package project.noticeboard.domain.main;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import project.noticeboard.domain.board.Board;
+import project.noticeboard.domain.board.repository.service.BoardService;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 public class MainController {
+    private final BoardService boardService;
     //메인화면 조회
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        List<Board> boards =boardService.findAll();
+        model.addAttribute("boards", boards);
         return "index";
     }
 }
