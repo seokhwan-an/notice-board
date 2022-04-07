b9de09e8f8057e6c410d6a7a16bb6d715caa758c
SeokHwan An <asok369258@naver.com>
2022-04-07 17:14:28 +0900

    PostService @RequiredArgsConstructor 추가 및 delete 매소드 수정

--- src/main/java/project/noticeboard/domain/post/service/PostService.java
+++ src/main/java/project/noticeboard/domain/post/service/PostService.java
@@ -1,5 +1,8 @@
 package project.noticeboard.domain.post.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
 import project.noticeboard.domain.post.Post;
 import project.noticeboard.domain.post.repository.PostRepository;
 
@@ -7,14 +10,13 @@ import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
+
 @Transactional
+@RequiredArgsConstructor
+@Service
 public class PostService {
     private final PostRepository postRepository;
-    //DI 가능하게
-    public PostService(PostRepository postRepository){
-        this.postRepository = postRepository;
-    }
-    // 저장 기능
+
     public Long save(Post post){
         postRepository.save(post);
         return post.getId();
@@ -24,25 +26,25 @@ public class PostService {
         Optional<Post> get = Optional.ofNullable(postRepository.findById(id)).get();
         return get;
     }
-    // 제목 검색 기능
-    public Optional<Post> findbyTitle(Post post){
-         Optional<Post> result = Optional.ofNullable(postRepository.findByTitle(post.getTitle()).get());
-         return result;
-    }
+//     제목 검색 기능
+//    public Optional<Post> findbyTitle(Post post){
+//         Optional<Post> result = Optional.ofNullable(postRepository.findByTitle(post.getTitle()).get());
+//         return result;
+//    }
+//
+//     작성자 검색 기능
+//    public Optional<Post> findbyWriter(Post post){
+//        Optional<Post> result = Optional.ofNullable(postRepository.findByWriter(post.getWriter()).get());
+//        return result;
+//    }
 
-    // 작성자 검색 기능
-    public Optional<Post> findbyWriter(Post post){
-        Optional<Post> result = Optional.ofNullable(postRepository.findByWriter(post.getWriter()).get());
-        return result;
-    }
-
-    // 모든 글 반환 기능
+//     모든 글 반환 기능
     public List<Post> findPosts(){
         List<Post> result = postRepository.findAll();
         return result;
     }
-    // 게시글 삭제
+//     게시글 삭제
     public void delete(Long id){
-        postRepository.delete(id);
+        postRepository.deleteById(id);
     }
 }
