e3fa5bf5c407c83edb7f4b50364e31b266df57de
SeokHwan An <asok369258@naver.com>
2022-04-09 22:52:03 +0900

    BoardRepository 인터페이스 수정

--- src/main/java/project/noticeboard/domain/board/repository/BoardRepository.java
+++ src/main/java/project/noticeboard/domain/board/repository/BoardRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import project.noticeboard.domain.board.Board;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
-
+    public Board getByType(String type);
 }
