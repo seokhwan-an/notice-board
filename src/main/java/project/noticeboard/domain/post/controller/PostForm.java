6d592ef007c547b7b27da25d71408b00b1f3c47b
SeokHwan An <asok369258@naver.com>
2022-04-10 11:58:36 +0900

    PostForm 필요없는 부분 코드 삭제

--- src/main/java/project/noticeboard/domain/post/controller/PostForm.java
+++ src/main/java/project/noticeboard/domain/post/controller/PostForm.java
@@ -4,10 +4,6 @@ public class PostForm {
     private String title;
     private String writer;
     private String body;
-    private long board_id;
-
-    public long getBoard_id() {return board_id;}
-    public void setBoard_id(long board_id) {this.board_id = board_id;}
 
     public String getTitle(){
         return title;
